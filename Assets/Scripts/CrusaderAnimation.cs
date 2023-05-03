b6a2e172f5e524f3910dff97b1ee8fd0279ce3de
jackyy2 <jackyyu2002@nevada.unr.edu>
2023-05-03 12:19:04 -0700

    Knight has movement animation
    Knight animation now looks like it is moving somewhat properly.

--- Assets/Scripts/CrusaderAnimation.cs
+++ Assets/Scripts/CrusaderAnimation.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CrusaderAnimation : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("WalkingF",true);
+        }
+        if(!Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("WalkingF",false);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("WalkingB",true);
+        }
+        if(!Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("WalkingB",false);
+        }
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("Striding",true);
+        }
+        if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("Striding",false);
+        }
     }
 }

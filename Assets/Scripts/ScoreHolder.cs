e259442d6675a6b704b61c67062a80eef9ef725e
vha <70501200+net-vha@users.noreply.github.com>
2023-05-15 08:22:38 -0700

    scores shown

--- Assets/Scripts/ScoreHolder.cs
+++ Assets/Scripts/ScoreHolder.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreHolder : MonoBehaviour
 {
+    ScoreHolder inst;
     public int currentRound;
     public int maxRounds;
     private int[] scores;
@@ -13,13 +15,23 @@ public class ScoreHolder : MonoBehaviour
     {
         DontDestroyOnLoad(this.gameObject);
         scores = new int[maxRounds];
+        inst = this;
     }
 
     void Start()
     {
-        for(int count = 0; count < maxRound; count++)
+        for(int count = 0; count < maxRounds; count++)
         {
             scores[count] = 0;
+        }
+    }
+
+    void Update()
+    {
+        if(GameObject.Find("ScoreText") != null)
+        {
+            GameObject.Find("ScoreText").GetComponent<Text>().text = "Encounter 1: " + scores[0] + "\n" + 
+                                                                     "Encounter 2: " + scores[1]; 
         }
     }
 

33a55417d10da45951f4f064ddcd01628afde158
vha <70501200+net-vha@users.noreply.github.com>
2023-05-15 04:16:33 -0700

    scene 2

--- Assets/Scripts/gameOver.cs
+++ Assets/Scripts/gameOver.cs
@@ -6,15 +6,25 @@ using UnityEngine.SceneManagement;
 public class gameOver : MonoBehaviour
 {
     public float timerEndScene = 30f;
+    private float remainingTime;
+    private int rounds = 2;
+    private int currentRound = 1;
+    private int[] scores;
+
     // Start is called before the first frame update
     void Start()
     {
+        remainingTime = timerEndScene;
         StartCoroutine(endScene_timer());
     }
 
     IEnumerator endScene_timer()
     {
-        yield return new WaitForSeconds(timerEndScene);
+        while(remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            yield return null;
+        }
 
         nextScene(5);
     }
@@ -22,5 +32,12 @@ public class gameOver : MonoBehaviour
     public void nextScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void saveScore(float time, float playerHP, bool winCon)
+    {
+        scores[currentRound] = time*3 + playerHP;
+        if(winCon)
+            scores[currentRound] += 100;
     }
 }
